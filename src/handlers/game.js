bb7a4cfbd46b4065a10a001d11450dd35cf6e784
Vismaya P S <vismayaps@Vismayas-MacBook-Pro.local>
2026-03-31 16:09:45 +0530

    [#9] | Refactors game initialization and removes duplicate handlers | vismaya/mohanthi
    * Removes createGameInstance from game handler to avoid duplication
    * Eliminates redundant startGame and getGameState implementations from utils

--- src/handlers/game.js
+++ src/handlers/game.js
@@ -1,30 +1,5 @@
-import { boardConfig } from "../constants/board_config.js";
-import { PAWNS, ROOMS, SUSPECTS, WEAPONS } from "../constants/game_config.js";
-import { Board } from "../models/board.js";
-import { DeckManager } from "../models/deck_manager.js";
-import { Game } from "../models/game.js";
-import { Pawn } from "../models/pawn.js";
-
-const createPawns = () =>
-  PAWNS
-    .map(({ name, position, color }, index) =>
-      new Pawn(index + 1, name, position, color)
-    );
-
-export const createGameInstance = () => {
-  const board = new Board(boardConfig);
-  const pawns = createPawns();
-  const deck = new DeckManager({
-    suspects: SUSPECTS,
-    weapons: WEAPONS,
-    rooms: ROOMS,
-  });
-  return new Game(1, board, pawns, deck);
-};
-
 export const startGame = (c) => {
   const game = c.get("game");
-
   game.changeCurrentState();
   game.distributeCards();
 
--- src/utils/game.js
+++ src/utils/game.js
@@ -21,18 +21,3 @@ export const createGameInstance = () => {
   });
   return new Game(1, board, pawns, deck);
 };
-
-export const startGame = (c) => {
-  const game = c.get("game");
-
-  game.changeCurrentState();
-  game.distributeCards();
-
-  return c.redirect("/pages/setup.html", 303);
-};
-
-export const getGameState = (c) => {
-  const game = c.get("game");
-  const currentState = game.getCurrentState();
-  return c.json(currentState, 200);
-};
